7be4a7bf43561f06db0bc221e57cbe1c8bdfc140
YuvrajKathpalia <yuvraj.kathpalia20@gmail.com>
2024-12-22 20:40:01 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 
 dotenv.config();
@@ -16,7 +17,11 @@ app.use(express.json());
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/trips', require('./routes/tripRoutes'));
+app.use('/api/bookings',require('./routes/bookingroutes'));
 
-const PORT = process.env.PORT || 5000;
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 2000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
